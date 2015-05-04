8b4a6756826c58636925e6dea7e5dd458f01a971
Sean Lang <slang800@gmail.com>
2015-05-03 22:51:41 -0400

    rename vars to camelCase to match JS functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var path = require("path");
-var max_bytes = 512;
+var maxBytes = 512;
 
 module.exports = function(bytes, size) {
   var file = bytes;
@@ -13,7 +13,7 @@ module.exports = function(bytes, size) {
     }
     var descriptor = fs.openSync(file, 'r');
     try {
-      bytes = new Buffer(max_bytes);
+      bytes = new Buffer(maxBytes);
       size = fs.readSync(descriptor, bytes, 0, bytes.length, 0);
     } finally {
       fs.closeSync(descriptor);
@@ -27,7 +27,7 @@ module.exports = function(bytes, size) {
 
       fs.open(file, 'r', function(err, descriptor){
           if (err) return callback(err);
-          var bytes = new Buffer(max_bytes);
+          var bytes = new Buffer(maxBytes);
           // Read the file with no encoding for raw buffer access.
           fs.read(descriptor, bytes, 0, bytes.length, 0, function(err, size, bytes){
             fs.close(descriptor, function(err2){
@@ -47,42 +47,42 @@ function isBinaryCheck(bytes, size) {
   if (size === 0)
     return false;
 
-  var suspicious_bytes = 0;
-  var total_bytes = Math.min(size, max_bytes);
+  var suspiciousBytes = 0;
+  var totalBytes = Math.min(size, maxBytes);
 
   if (size >= 3 && bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF) {
     // UTF-8 BOM. This isn't binary.
     return false;
   }
 
-  for (var i = 0; i < total_bytes; i++) {
+  for (var i = 0; i < totalBytes; i++) {
     if (bytes[i] === 0) { // NULL byte--it's binary!
       return true;
     }
     else if ((bytes[i] < 7 || bytes[i] > 14) && (bytes[i] < 32 || bytes[i] > 127)) {
       // UTF-8 detection
-      if (bytes[i] > 193 && bytes[i] < 224 && i + 1 < total_bytes) {
+      if (bytes[i] > 193 && bytes[i] < 224 && i + 1 < totalBytes) {
           i++;
           if (bytes[i] > 127 && bytes[i] < 192) {
               continue;
           }
       }
-      else if (bytes[i] > 223 && bytes[i] < 240 && i + 2 < total_bytes) {
+      else if (bytes[i] > 223 && bytes[i] < 240 && i + 2 < totalBytes) {
           i++;
           if (bytes[i] > 127 && bytes[i] < 192 && bytes[i + 1] > 127 && bytes[i + 1] < 192) {
               i++;
               continue;
           }
       }
-      suspicious_bytes++;
+      suspiciousBytes++;
       // Read at least 32 bytes before making a decision
-      if (i > 32 && (suspicious_bytes * 100) / total_bytes > 10) {
+      if (i > 32 && (suspiciousBytes * 100) / totalBytes > 10) {
           return true;
       }
     }
   }
 
-  if ((suspicious_bytes * 100) / total_bytes > 10) {
+  if ((suspiciousBytes * 100) / totalBytes > 10) {
     return true;
   }
 
